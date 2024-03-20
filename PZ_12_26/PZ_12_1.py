9216e98d2b9c31670355b19478d912758aeb5e13
w00xy <vlad42006@mail.com>
2024-03-20 12:35:29 +0300

    PZ_13 pdf pz 13 pdf

--- PZ_12_26/PZ_12_1.py
+++ PZ_12_26/PZ_12_1.py
@@ -1,16 +1,14 @@
 # В последовательности их N чисел (N –четное) во второй ее половине найти сумму
 # элементов больших 10.
+import random
 
 def sum_elements_greater_than_10(sequence):
-    mid = len(sequence) // 2
-    second_half = sequence[mid:]
-    filtered_elements = filter(lambda x: x > 10, second_half)
-    sum_of_elements = sum(filtered_elements)
-    return sum_of_elements
+    return sum([x for x in sequence[len(sequence) // 2:] if x > 10])
 
-sequence = [5, 12, 3, 8, 20, 15, 10, 25, 7, 30]  # пример последовательности чисел
+
+sequence = [random.randint(-1, 12) for i in range(10)]  # пример последовательности чисел
 N = len(sequence) # количество элементов в последовательности
-
+print(f'{sequence} - сгенерированный список')
 if len(sequence) % 2 == 0:  # проверка на четность числа N
     result = sum_elements_greater_than_10(sequence)
     print("Сумма элементов больше 10 во второй половине последовательности:", result)
