c9aff31d664f903025e8e2da0a002ce702462b1b
peresvet53466 <peresvetponomarev47@gmail.com>
2024-03-27 13:37:16 +0300

    PZ_14 and exercise regular

--- PZ_14_26/PZ_14_26_1.py
+++ PZ_14_26/PZ_14_26_1.py
@@ -1,4 +1,17 @@
+import re
 # В исходном текстовом файле(Dostoevsky.txt) найти все варианты фамилии
 # Достоевского (т.е. с различными окончаниями, например, Достоевский,
 # Достоевского) в единственном экземпляре. 
+
+
+with open('Dostoevsky.txt', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+surname_occurrences = re.findall(r'\bДостоевск\w+', text)
+unique_surnames = set(surname_occurrences)
+
+print(f'количество совпадений: {len(unique_surnames)}')
+
+for surname in unique_surnames:
+    print(surname)
 
